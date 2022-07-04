cfff6f8345f1a4745130f020e8c4094001565e4b
dashu <bigtree@gmail.com>
2022-07-04 15:11:17 +0700

    fix：登录对接数据库；

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use function MongoDB\BSON\toJSON;
 
 class LoginController extends Controller
 {
@@ -14,6 +18,9 @@ class LoginController extends Controller
         return view('admin.login.login');
     }
 
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function login(Request $request)
     {
         // 表单验证
@@ -24,9 +31,31 @@ class LoginController extends Controller
             'username.required' => '请输入正确的账号和密码！',
 //            'password.required' => '请输入正确的账号和密码！'
         ]);
-        dump($post);
-//        dump($request->all());
-//        return view('admin.index');
+        //验证登录
+        if (Auth::attempt($post, $request->has('remember'))) {
+
+            if (Auth::user()->activated == '1') {
+                session()->flash('success', '欢迎回来！');
+                return redirect()->intended(route('admin.index', [Auth::user()]));
+            } else {
+                Auth::logout();
+                return redirect('/');
+            }
+        } else {
+            // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的账号和密码不匹配');
+            return redirect()->back();
+
+        }
 
     }
+
+
+    public function end()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,38 +9,39 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-//    use HasApiTokens, HasFactory, Notifiable;
-//
-//    /**
-//     * The attributes that are mass assignable.
-//     *
-//     * @var array<int, string>
-//     */
-//    protected $fillable = [
-//        'name',
-//        'email',
-//        'password',
-//    ];
-//
-//    /**
-//     * The attributes that should be hidden for serialization.
-//     *
-//     * @var array<int, string>
-//     */
-//    protected $hidden = [
-//        'password',
-//        'remember_token',
-//    ];
-//
-//    /**
-//     * The attributes that should be cast.
-//     *
-//     * @var array<string, string>
-//     */
-//    protected $casts = [
-//        'email_verified_at' => 'datetime',
-//    ];
-    // 拒绝添加的字段
-    protected $guarded = [];
+    use HasApiTokens, HasFactory, Notifiable;
+
+    //关联表
+    protected $table = 'users';
+    /**
+     * The attributes that are mass assignable.
+     * 可添加字段
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'activated',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
 }
